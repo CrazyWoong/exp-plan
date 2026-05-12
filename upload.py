4611c3cc7c71705e376ffaf6a56ba14463004c4d
CrazyWoong <20500094@hankooktech.com>
2026-05-12 13:53:28 +0900

    temp commit for rebase

--- upload.py
+++ upload.py
@@ -29,3 +29,10 @@ os.system("git push")
 print(f"{dot}주차 파일 업로드 완료!")
 
 # cmd 에서 해당 위치 찾아가서 실행 upload : cd OneDrive - HKNC\Streamlit Python
+
+# 에러 발생 시
+# git add .
+# git commit -m "temp commit for rebase"
+
+# git pull origin main --rebase
+# git push origin main
